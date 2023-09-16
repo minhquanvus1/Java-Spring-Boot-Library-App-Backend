3d4d4dad44df5891a1f4bd0748928e70afebbd99
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-16 21:08:14 +0700

    configure READ-ONLY permission for Book entity, and set CORS Mapping for the React app

---
+++ src/main/java/com/luv2code/springbootlibrary/config/MyDataRestConfig.java
@@ -0,0 +1,39 @@
+package com.luv2code.springbootlibrary.config;
+
+import com.luv2code.springbootlibrary.entity.Book;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Value("${allowed.origins}")
+    private String[] theAllowedOrigin;
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+
+        HttpMethod[] theUnsupportedMethods = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.PATCH};
+        disableHttpMethods(Book.class, config, theUnsupportedMethods);
+
+        // expose "id" field for Book entity
+        config.exposeIdsFor(Book.class);
+
+        // configure CORS Mapping for Spring Data Rest
+        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigin);
+    }
+
+    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedMethods) {
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethods))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethods));
+
+
+    }
+}
