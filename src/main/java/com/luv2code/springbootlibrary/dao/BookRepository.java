bab53800b057247886daf6573d67e944e2671a15
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-16 20:38:35 +0700

    create Book Repository

---
+++ src/main/java/com/luv2code/springbootlibrary/dao/BookRepository.java
@@ -0,0 +1,9 @@
+package com.luv2code.springbootlibrary.dao;
+
+import com.luv2code.springbootlibrary.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
