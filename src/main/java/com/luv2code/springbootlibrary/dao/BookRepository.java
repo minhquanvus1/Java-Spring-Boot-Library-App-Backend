0b07e9d3351f447f5e4d09b175bf85f26a5047d5
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-17 09:06:30 +0700

    define search query to find Book that has particular title, with Pagination support

--- src/main/java/com/luv2code/springbootlibrary/dao/BookRepository.java
+++ src/main/java/com/luv2code/springbootlibrary/dao/BookRepository.java
@@ -1,9 +1,15 @@
 package com.luv2code.springbootlibrary.dao;
 
 import com.luv2code.springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RepositoryRestResource
 public interface BookRepository extends JpaRepository<Book, Long> {
+    // available at: http://localhost:8080/api/books/search/findByTitleContaining{?title=}
+    // the SAME as: select * from book where title:= title;
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
