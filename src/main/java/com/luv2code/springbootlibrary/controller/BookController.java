beb98d62784881976bcfff87e85dc7beefbfd0ea
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-18 10:56:09 +0700

    create BookController to call BookService to allow user to checkout book

---
+++ src/main/java/com/luv2code/springbootlibrary/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.luv2code.springbootlibrary.controller;
+
+import com.luv2code.springbootlibrary.entity.Book;
+import com.luv2code.springbootlibrary.service.BookService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    private BookService bookService;
+
+    // constructor dependency injection
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
+        String userEmail = "john.doe@test.com";
+        return bookService.checkoutBook(userEmail, bookId);
+
+    }
+}
