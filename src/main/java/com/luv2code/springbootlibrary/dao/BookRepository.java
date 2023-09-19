c6c4ce5f96f0cccd1656dc9602e0e5e4b4fdb8f0
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-19 11:25:19 +0700

    define a book service to return list of books that a user is currently checking out/borrowing, with daysLeft for each book. And define a search query (JPQL) to get list of books that has id IN a list of ids

--- src/main/java/com/luv2code/springbootlibrary/dao/BookRepository.java
+++ src/main/java/com/luv2code/springbootlibrary/dao/BookRepository.java
@@ -4,8 +4,12 @@ import com.luv2code.springbootlibrary.entity.Book;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface BookRepository extends JpaRepository<Book, Long> {
@@ -17,4 +21,8 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     // the SAME as: select * from book where category:= category;
 
     Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
+
+    @Query("select o from Book o where id in :book_ids")
+    // select each book record in Book table, that this book has id in this list of book_ids;
+    List<Book> findBooksByBookIds(@Param("book_ids") List<Long> bookIds);
 }
--- src/main/java/com/luv2code/springbootlibrary/service/BookService.java
+++ src/main/java/com/luv2code/springbootlibrary/service/BookService.java
@@ -5,11 +5,17 @@ import com.luv2code.springbootlibrary.dao.BookRepository;
 import com.luv2code.springbootlibrary.dao.CheckoutRepository;
 import com.luv2code.springbootlibrary.entity.Book;
 import com.luv2code.springbootlibrary.entity.Checkout;
+import com.luv2code.springbootlibrary.responsemodels.ShelfCurrentLoansResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Transactional
@@ -67,5 +73,51 @@ public class BookService {
     // count # of checkouts that a user with this userEmail has checked out (by finding the size() of the returned list of checkouts that a user with this userEmail has checked out)
     public int currentLoansCount(String userEmail) {
         return checkoutRepository.findBooksByUserEmail(userEmail).size();
+    }
+
+    // return the List of ShelfCurrentLoansResponse (Books that this user is in the process of checking out/borrowing, with the daysLeft for returning of each book)
+    public List<ShelfCurrentLoansResponse> currentLoans(String userEmail) throws Exception {
+        // create an empty list, which is also the list that this function will return at the end
+        List<ShelfCurrentLoansResponse> shelfCurrentLoansResponses = new ArrayList<>();
+
+        // return a list of Checkout objects representing Books that this user has checked out
+        List<Checkout> checkoutList = checkoutRepository.findBooksByUserEmail(userEmail);
+
+        // because the ShelfCurrentLoansResponse object requires the 1st parameter as the Book object (not Checkout object) --> we need to convert a Checkout object into Book object, by using the id
+        // an arrayList to store all bookId of all Checkouts books that this user has checked out
+        List<Long> bookIdList = new ArrayList<>();
+
+        // extract all the bookId of each Checkout object, and add into bookIdList arrayList
+        // ---> after this for loop, we get: the list of book ids representing books that this user is currently checking out/borrowing
+        for(Checkout i : checkoutList) {
+            bookIdList.add(i.getBookId());
+        }
+        // return list of books (Book objects) that has id in this list of bookIdList
+        // ---> we get: the list of Books (Book objects, not Checkout objects anymore) that this user is currently checking out/borrowing
+        List<Book> books = bookRepository.findBooksByBookIds(bookIdList);
+
+        // is used to: compare dates, to see how many days left for this book to be returned, or how late the book is
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        // so at this time, we already get the list of Book objects that this user is borrowing (each Book object in this list is the 1st parameter of the ShelfCurrentLoansResponse object)
+        // we only need the (int) daysLeft for each Book object.
+        // However, the returnDate attribute ONLY belongs to the Checkout object (not of Book object)
+        // ---> We need to: loop through the list of Book objects --> find the only 1 matching Checkout object for this Book object --> if this matching Checkout object exists --> getReturnDate --> combine with LocalDate.now() to find the difference in milliseconds (using getTime()) --> then use TimeUnit.time to convert milliseconds to Days (in "long" data type, which may contain so thap phan) --> type cast to (int), because we need whole number for daysLeft
+        for(Book book: books) {
+            Optional<Checkout> checkout = checkoutList.stream().filter(x -> x.getBookId() == book.getId()).findFirst();
+            if(checkout.isPresent()) {
+                Date d1 = sdf.parse(checkout.get().getReturnDate());
+                Date d2 = sdf.parse(LocalDate.now().toString());
+
+                TimeUnit time = TimeUnit.DAYS;
+                long difference_In_Time = time.convert(d1.getTime() - d2.getTime(), TimeUnit.MILLISECONDS);
+
+                // for each Book object, initialize a new ShelfCurrentLoansResponse object, pass in that Book, and the difference_In_Time
+                // --> then, add this object into the list of ShelfCurrentLoansResponse objects, created at the beginning
+                shelfCurrentLoansResponses.add(new ShelfCurrentLoansResponse(book, (int) difference_In_Time));
+            }
+        }
+        // return the list of ShelfCurrentLoansResponse objects (list of books (with daysLeft for each book), that this user is checking out/borrowing)
+        return shelfCurrentLoansResponses;
     }
 }
