58218a1d90de7f4f7f5b23b58bc9e7aab547355a
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-20 21:54:46 +0700

    create a Rest endpoint to receive API call to PUT admin's response to normal user's question

--- src/main/java/com/luv2code/springbootlibrary/controller/MessagesController.java
+++ src/main/java/com/luv2code/springbootlibrary/controller/MessagesController.java
@@ -1,6 +1,7 @@
 package com.luv2code.springbootlibrary.controller;
 
 import com.luv2code.springbootlibrary.entity.Message;
+import com.luv2code.springbootlibrary.requestmodels.AdminQuestionRequest;
 import com.luv2code.springbootlibrary.service.MessagesService;
 import com.luv2code.springbootlibrary.utils.ExtractJWT;
 import org.springframework.web.bind.annotation.*;
@@ -20,5 +21,17 @@ public class MessagesController {
     public void postMessage(@RequestHeader(value = "Authorization") String token, @RequestBody Message messageRequest) {
 String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
 messagesService.postMessage(messageRequest, userEmail);
+    }
+
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value = "Authorization") String token, @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        // check if the user calls this API is actually an admin (only admin can call this API)
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if(admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only!");
+        }
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        messagesService.putMessage(adminQuestionRequest, userEmail);
+
     }
 }
