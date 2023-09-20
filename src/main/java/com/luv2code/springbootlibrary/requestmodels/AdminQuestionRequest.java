652b3f7cfd70c181331fb32bab0eedcc7fde41c2
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-20 21:28:42 +0700

    create an AdminQuestionRequest

---
+++ src/main/java/com/luv2code/springbootlibrary/requestmodels/AdminQuestionRequest.java
@@ -0,0 +1,13 @@
+package com.luv2code.springbootlibrary.requestmodels;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdminQuestionRequest {
+
+    private Long id;
+
+    private String response;
+}
