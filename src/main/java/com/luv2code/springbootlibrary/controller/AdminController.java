9244a1ebd9dbe1a3dde4b4ab3a003e22d3da6ef7
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-21 06:52:27 +0700

    create a Rest endpoint to receive request to addBook, and configure Security so that ONLY authenticated user can call /api/admin/secure/** API

---
+++ src/main/java/com/luv2code/springbootlibrary/controller/AdminController.java
@@ -0,0 +1,29 @@
+package com.luv2code.springbootlibrary.controller;
+
+import com.luv2code.springbootlibrary.requestmodels.AddBookRequest;
+import com.luv2code.springbootlibrary.service.AdminService;
+import com.luv2code.springbootlibrary.utils.ExtractJWT;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    private AdminService adminService;
+
+    // constructor dependency injection
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/secure/add/book")
+    public void postBook(@RequestHeader(value = "Authorization") String token, @RequestBody AddBookRequest addBookRequest) throws Exception {
+        // check if the user who calls this API, is actually an admin or not (ONLY admin can call this API)
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if(!admin.equals("admin") || admin == null) {
+            throw new Exception("Administration page only!");
+        }
+        adminService.postBook(addBookRequest);
+    }
+}
