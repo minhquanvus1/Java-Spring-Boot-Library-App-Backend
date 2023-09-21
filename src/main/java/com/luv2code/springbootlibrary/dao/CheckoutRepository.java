172f7bcdc27f0b507f430bc41f3f06d8197588d6
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-21 16:59:02 +0700

    update CheckoutRepository and ReviewRepository so that when admin delete a book, we will delete all records in having book_id == this book_id, in checkout table, and review table

--- src/main/java/com/luv2code/springbootlibrary/dao/CheckoutRepository.java
+++ src/main/java/com/luv2code/springbootlibrary/dao/CheckoutRepository.java
@@ -2,6 +2,9 @@ package com.luv2code.springbootlibrary.dao;
 
 import com.luv2code.springbootlibrary.entity.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
@@ -16,4 +19,9 @@ public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
     // return list of Checkout objects (books that has been checked out) that a particular user (with the particular email) has checked out
    // the SAME as: select * from checkout where user_email := email;
     List<Checkout> findBooksByUserEmail(String email);
+
+    @Modifying // add @Modidying annotation: because we're deleting records in database --> we're adding a query to MODIFY the database
+    @Query("delete from checkout where book_id := book_id")
+    // here, we want that: if admin deletes a book, then we want to delete all records in checkout table whose book_id == book_id of this book
+    void deleteAllByBookId(@Param("book_id") Long bookId);
 }
--- src/main/java/com/luv2code/springbootlibrary/dao/ReviewRepository.java
+++ src/main/java/com/luv2code/springbootlibrary/dao/ReviewRepository.java
@@ -4,6 +4,9 @@ import com.luv2code.springbootlibrary.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,4 +18,9 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findByBookId(@RequestParam("book_id") Long bookId, Pageable pageable);
 
     Review findByUserEmailAndBookId(String userEmail, Long bookId);
+
+    @Modifying // add @Modidying annotation: because we're deleting records in database --> we're adding a query to MODIFY the database
+    @Query("delete from review where book_id = book_id")
+        // here, we want that: if admin deletes a book, then we want to delete all records in review table whose book_id == book_id of this book
+    void deleteAllByBookId(@Param("book_id") Long book_id);
 }
