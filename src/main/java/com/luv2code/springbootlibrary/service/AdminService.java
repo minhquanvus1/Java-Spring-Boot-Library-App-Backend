3fc8878e021991be3399a2a57f5306b51bc054d2
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-21 12:26:50 +0700

    create a function to let admin increase quantity of a particular book

--- src/main/java/com/luv2code/springbootlibrary/service/AdminService.java
+++ src/main/java/com/luv2code/springbootlibrary/service/AdminService.java
@@ -6,6 +6,8 @@ import com.luv2code.springbootlibrary.requestmodels.AddBookRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 public class AdminService {
@@ -28,5 +30,20 @@ public class AdminService {
         book.setCategory(addBookRequest.getCategory());
 
         bookRepository.save(book);
+    }
+
+    // create a function to allow admin to increase quantity of a particular book
+    public void increaseBookQuantity(Long bookId) throws Exception {
+        // check if this book does exist in the database or not (because we can't increase quantity of a non-exist book)
+        Optional<Book> book = bookRepository.findById(bookId);
+        if(!book.isPresent()) {
+            throw new Exception("Book does not exist");
+        }
+        // increase quantity of a book == increase its total copies, and increase its total copiesAvailable
+        book.get().setCopies(book.get().getCopies() + 1);
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() + 1);
+
+        // remember to save this book (with updated infor) back to the database
+        bookRepository.save(book.get());
     }
 }
