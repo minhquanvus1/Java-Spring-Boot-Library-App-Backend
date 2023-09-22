c0b4ad613bfd24c529c9baae9d55b164c44d0d00
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-22 11:44:54 +0700

    create a function in PaymentService to set the amount (amount of money that a user has to pay, due to late book return) to 0.00, after he has already paid the fee

--- src/main/java/com/luv2code/springbootlibrary/service/PaymentService.java
+++ src/main/java/com/luv2code/springbootlibrary/service/PaymentService.java
@@ -1,11 +1,14 @@
 package com.luv2code.springbootlibrary.service;
 
 import com.luv2code.springbootlibrary.dao.PaymentRepository;
+import com.luv2code.springbootlibrary.entity.Payment;
 import com.luv2code.springbootlibrary.requestmodels.PaymentInfoRequest;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,5 +40,19 @@ public class PaymentService {
         params.put("payment_method_types", paymentMethodTypes);
 
         return PaymentIntent.create(params);
+    }
+
+    // create a function to set the amount (amount of money that a user has to paid, due to late book return) to 0.00, when he has already paid the fee
+    public ResponseEntity<String> stripePayment(String userEmail) throws Exception {
+        // check in the payment table in database if this user has the payment needed to be charged (because we can't charge a user if he isn't in the payment table where he has to pay fee)
+        Payment payment = paymentRepository.findByUserEmail(userEmail);
+        if(payment == null) {
+            throw new Exception("Payment information is missing!");
+        }
+        // after this user has successfully made the payment, then, set his amount (amount of money he has to pay due to late book return) to 0 (becasue he has already paid the fee)
+        payment.setAmount(0.00);
+        // save this payment with the updated infor to the database
+        paymentRepository.save(payment);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
