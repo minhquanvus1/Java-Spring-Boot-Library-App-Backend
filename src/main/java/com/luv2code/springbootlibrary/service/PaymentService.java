0d173c741fc4f19698a8e259b1272616f118f038
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-22 11:30:58 +0700

    create a function in PaymentService to create PaymentIntent with the help of Stripe method

--- src/main/java/com/luv2code/springbootlibrary/service/PaymentService.java
+++ src/main/java/com/luv2code/springbootlibrary/service/PaymentService.java
@@ -1,10 +1,18 @@
 package com.luv2code.springbootlibrary.service;
 
 import com.luv2code.springbootlibrary.dao.PaymentRepository;
+import com.luv2code.springbootlibrary.requestmodels.PaymentInfoRequest;
 import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @Transactional
@@ -16,5 +24,18 @@ public class PaymentService {
     public PaymentService(PaymentRepository paymentRepository, @Value("${stripe.key.secret}") String secretKey) {
         this.paymentRepository = paymentRepository;
         Stripe.apiKey = secretKey;
+    }
+
+    // create a PaymentService function to create PaymentIntent, by: receiving PaymentInfoRequest object from controller (which got it from frontend API call), extract necessary data, and put it in Map object, then call method of Stripe to create PaymentIntent
+    public PaymentIntent createPaymentIntent(PaymentInfoRequest paymentInfoRequest) throws StripeException {
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfoRequest.getAmount());
+        params.put("currency", paymentInfoRequest.getCurrency());
+        params.put("payment_method_types", paymentMethodTypes);
+
+        return PaymentIntent.create(params);
     }
 }
